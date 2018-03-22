b6d55882db55da0261f15654b774ce27f6e5c138
Xinyu Liu <meteor.lxy@foxmail.com>
2018-03-22 10:22:00 +0000

    chore: add port in url generation

--- src/CheckJetbrains.php
+++ src/CheckJetbrains.php
@@ -45,7 +45,7 @@ class CheckJetbrains extends Check implements CheckInterface {
      * @return \Xjtuana\HealthCheck\Result
      */
     public function checkAtOnce(string $addr, int $port = 0, int $timeout = 3): Result {
-        $res = $this->http()->get(0 === strpos($addr, 'http') ? $addr : 'http://' . $addr, [
+        $res = $this->http()->get((0 === strpos($addr, 'http') ? $addr : 'http://' . $addr) . ($port ? ':' . $port : ''), [
             'timeout' => $timeout,
             'connect_timeout' => $timeout,
         ])->getBody()->getContents();
